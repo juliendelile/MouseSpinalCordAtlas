9ad41450ee8791826ace00a77c1d95ca9bcba131
juliendelile <julien.delile@gmail.com>
2018-11-26 17:21:56 +0000

    add modified monocle functions

--- R_files/MouseSpinalCordAtlas_tools.R
+++ R_files/MouseSpinalCordAtlas_tools.R
@@ -1020,3 +1020,207 @@ plotNeurogenesisHeatmap <- function(df.merge, num_pt, plotted_DV, pt_clusters_re
   graphics.off()
 }
 
+
+my_diff_test_helper <- function(x, 
+                             fullModelFormulaStr, 
+                             reducedModelFormulaStr, 
+                             expressionFamily, 
+                             relative_expr,
+                             weights,
+                             disp_func=NULL,
+                             verbose=FALSE
+                             ){ 
+  
+  reducedModelFormulaStr <- paste("f_expression", reducedModelFormulaStr, sep="")
+  fullModelFormulaStr <- paste("f_expression", fullModelFormulaStr, sep="")
+  
+  x_orig <- x
+  disp_guess <- 0
+  
+  if (expressionFamily@vfamily %in% c("negbinomial", "negbinomial.size")){
+    if (relative_expr == TRUE)
+    {
+      x <- x / Size_Factor
+    }
+    f_expression <- round(x)
+    if (is.null(disp_func) == FALSE){
+
+      # browser()
+      
+      disp_guess <- monocle:::calculate_NB_dispersion_hint(disp_func, round(x_orig))
+      if (is.null(disp_guess) == FALSE && disp_guess > 0 && is.na(disp_guess) == FALSE  ) {
+        # FIXME: In theory, we could lose some user-provided parameters here
+        # e.g. if users supply zero=NULL or something. 
+        if (expressionFamily@vfamily == "negbinomial")
+          expressionFamily <- VGAM::negbinomial(isize=1/disp_guess)
+        else
+          expressionFamily <- VGAM::negbinomial.size(size=1/disp_guess)
+      }
+    }
+  }else if (expressionFamily@vfamily %in% c("gaussianff", "uninormal")){
+    f_expression <- x
+  }else if (expressionFamily@vfamily %in% c("binomialff")){
+    f_expression <- x
+    #f_expression[f_expression > 1] <- 1
+  }else{
+    f_expression <- log10(x)
+  }
+  
+  test_res <- tryCatch({
+    if (expressionFamily@vfamily %in% c("binomialff")){
+      if (verbose){
+        full_model_fit <- VGAM::vglm(as.formula(fullModelFormulaStr), epsilon=1e-1, family=expressionFamily)
+        reduced_model_fit <- VGAM::vglm(as.formula(reducedModelFormulaStr), epsilon=1e-1, family=expressionFamily)                         
+      }else{
+        full_model_fit <- suppressWarnings(VGAM::vglm(as.formula(fullModelFormulaStr), epsilon=1e-1, family=expressionFamily))
+        reduced_model_fit <- suppressWarnings(VGAM::vglm(as.formula(reducedModelFormulaStr), epsilon=1e-1, family=expressionFamily))                    
+      }
+    }else{
+      if (verbose){
+        full_model_fit <- VGAM::vglm(as.formula(fullModelFormulaStr), epsilon=1e-1, family=expressionFamily)
+        reduced_model_fit <- VGAM::vglm(as.formula(reducedModelFormulaStr), epsilon=1e-1, family=expressionFamily)                         
+      }else{
+        full_model_fit <- suppressWarnings(VGAM::vglm(as.formula(fullModelFormulaStr), epsilon=1e-1, family=expressionFamily))
+        reduced_model_fit <- suppressWarnings(VGAM::vglm(as.formula(reducedModelFormulaStr), epsilon=1e-1, family=expressionFamily))                    
+      }
+    }
+
+    #print(full_model_fit)
+    #print(coef(reduced_model_fit))
+    my_compareModels(list(full_model_fit), list(reduced_model_fit))
+  }, 
+  #warning = function(w) { FM_fit },
+  error = function(e) { 
+    if(verbose)
+      print (e);
+      data.frame(status = "FAIL", family=expressionFamily@vfamily, pval=1.0, qval=1.0, full_model_loglikelihood=NA, reduced_model_loglikelihood=NA)
+    #data.frame(status = "FAIL", pval=1.0) 
+  }
+  )
+  test_res
+}
+
+my_compareModels <- function(full_models, reduced_models){
+  stopifnot(length(full_models) == length(reduced_models))
+  test_res <- mapply(function(x,y) { 
+    if (is.null(x) == FALSE && is.null(y) == FALSE) {
+      lrt <- VGAM::lrtest(x,y) 
+      pval=lrt@Body["Pr(>Chisq)"][2,]
+      family = x@family@vfamily
+      if (length(family) > 1)
+        family = family[1]
+      data.frame(status = "OK", family=family, pval=pval)
+    } else { data.frame(status = "FAIL", family=NA, pval=1.0) } 
+  } , full_models, reduced_models, SIMPLIFY=FALSE, USE.NAMES=TRUE)
+  
+  test_res <- do.call(rbind.data.frame, test_res)
+  test_res$qval <- p.adjust(test_res$pval, method="BH")
+
+  # Julien: Only change in functions -> add loglikelihood of both models in result
+  test_res$full_model_loglikelihood = unlist(lapply(full_models, function(l) l@criterion$loglikelihood))
+  test_res$reduced_model_loglikelihood = unlist(lapply(reduced_models, function(l) l@criterion$loglikelihood))
+
+  test_res
+}
+
+
+my_smartEsApply <- function(X, MARGIN, FUN, convert_to_dense, ...) {
+  parent <- environment(FUN)
+  if (is.null(parent))
+    parent <- emptyenv()
+  e1 <- new.env(parent=parent)
+  Biobase::multiassign(names(Biobase::pData(X)), Biobase::pData(X), envir=e1)
+  environment(FUN) <- e1
+  
+  # if (isSparseMatrix(exprs(X))){
+  #   res <- sparseApply(exprs(X), MARGIN, FUN, convert_to_dense, ...)
+  # }else{
+    res <- apply(Biobase::exprs(X), MARGIN, FUN, ...)
+  # }
+  
+  if (MARGIN == 1)
+  {
+    names(res) <- row.names(X)
+  }else{
+    names(res) <- colnames(X)
+  }
+
+  res
+}
+
+my_differentialGeneTest <- function(cds, 
+                                 fullModelFormulaStr="~sm.ns(Pseudotime, df=3)",
+                                 reducedModelFormulaStr="~1", 
+                                 relative_expr=TRUE,
+                                 cores=1, 
+                                 verbose=FALSE
+                                 ){
+  status <- NA
+  
+  if(class(cds)[1] != "CellDataSet") {
+    stop("Error cds is not of type 'CellDataSet'")
+  }
+  
+  all_vars <- c(all.vars(formula(fullModelFormulaStr)), all.vars(formula(reducedModelFormulaStr)))
+   
+  pd <- Biobase::pData(cds)
+  
+  for(i in all_vars) {
+    x <- pd[, i]
+    if(any((c(Inf, NaN, NA) %in% x))){
+      stop("Error: Inf, NaN, or NA values were located in pData of cds in columns mentioned in model terms")
+    }
+  }
+  
+  
+  if (relative_expr && cds@expressionFamily@vfamily %in% c("negbinomial", "negbinomial.size")){
+    if (is.null(sizeFactors(cds)) || sum(is.na(sizeFactors(cds)))){
+      stop("Error: to call this function with relative_expr==TRUE, you must first call estimateSizeFactors() on the CellDataSet.")
+    }
+  }
+  
+  # if (cores > 1){
+  #   diff_test_res<-mcesApply(cds, 1, my_diff_test_helper, 
+  #                            c("BiocGenerics", "VGAM", "Matrix"), 
+  #                            cores=cores, 
+  #                            fullModelFormulaStr=fullModelFormulaStr,
+  #                            reducedModelFormulaStr=reducedModelFormulaStr,
+  #                            expressionFamily=cds@expressionFamily,
+  #                            relative_expr=relative_expr,
+  #                            disp_func=cds@dispFitInfo[["blind"]]$disp_func,
+  #                            verbose=verbose
+  #                      #       ,
+  #                      # backup_method = backup_method, 
+  #                      # use_epislon = use_epislon, 
+  #                      # stepsize = stepsize
+  #                            )
+  #   diff_test_res
+  # }else{
+    diff_test_res<-my_smartEsApply(cds,1,my_diff_test_helper, 
+                                convert_to_dense=TRUE,
+                                fullModelFormulaStr=fullModelFormulaStr,
+                                reducedModelFormulaStr=reducedModelFormulaStr, 
+                                expressionFamily=cds@expressionFamily, 
+                                relative_expr=relative_expr,
+                                disp_func=cds@dispFitInfo[["blind"]]$disp_func,
+                                verbose=verbose
+                       #          ,
+                       # backup_method = backup_method, 
+                       # use_epislon = use_epislon,
+                       # stepsize = stepsize
+
+                                )
+    diff_test_res
+  # }
+  
+  diff_test_res <- do.call(rbind.data.frame, diff_test_res)
+
+  diff_test_res$qval <- 1
+  diff_test_res$qval[which(diff_test_res$status == 'OK')] <- p.adjust(subset(diff_test_res, status == 'OK')[, 'pval'], method="BH")
+  
+  diff_test_res <- merge(diff_test_res, Biobase::fData(cds), by="row.names")
+  row.names(diff_test_res) <- diff_test_res[, 1] #remove the first column and set the row names to the first column
+  diff_test_res[, 1] <- NULL 
+
+  diff_test_res[row.names(cds), ] # make sure gene name ordering in the DEG test result is the same as the CDS
+}
